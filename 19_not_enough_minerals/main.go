193c045769afbe26a32babb84374bb6ea171c32b
Rossh87 <hunter.dev.87@gmail.com>
2023-01-28 00:10:35 -0600

    day 19 both parts

--- 19_not_enough_minerals/main.go
+++ 19_not_enough_minerals/main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/samber/lo"
+	"golang.org/x/exp/slices"
 )
+
+func main() {
+	r, err := os.Open("./input.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(solvePartTwo(r))
+}
 
 type oreCost struct {
 	oreCost int
@@ -23,17 +38,19 @@ type geodeCost struct {
 	obsidianCost int
 }
 
+type botMaxes map[resource]int
+
 type blueprint struct {
 	id          int
 	oreBot      oreCost
 	clayBot     clayCost
 	obsidianBot obsidianCost
 	geodeBot    geodeCost
+	botMax      botMaxes
 }
 
-// type blueprints []blueprint
-
 func parseBlueprint(raw string) blueprint {
+	maxes := make(botMaxes)
 	exp := regexp.MustCompile(`\d+`)
 	matches := exp.FindAllString(raw, -1)
 	if len(matches) != 7 {
@@ -49,11 +66,18 @@ func parseBlueprint(raw string) blueprint {
 	bp := blueprint{}
 	bp.id = asInts[0]
 	bp.oreBot.oreCost = asInts[1]
+	maxes[ore] = asInts[1]
 	bp.clayBot.oreCost = asInts[2]
+	maxes[ore] = intMax(asInts[2], maxes[ore])
 	bp.obsidianBot.oreCost = asInts[3]
+	maxes[ore] = intMax(asInts[3], maxes[ore])
 	bp.obsidianBot.clayCost = asInts[4]
+	maxes[clay] = asInts[4]
 	bp.geodeBot.oreCost = asInts[5]
+	maxes[ore] = intMax(asInts[5], maxes[ore])
 	bp.geodeBot.obsidianCost = asInts[6]
+	maxes[obsidian] = asInts[6]
+	bp.botMax = maxes
 	return bp
 }
 
@@ -71,6 +95,27 @@ type botKind = resource
 
 type botCounts map[resource]int
 
+func sortMapKeys(m map[resource]int) []resource {
+	out := make([]resource, len(m))
+	pos := 0
+	for k := range m {
+		out[pos] = k
+		pos++
+	}
+	slices.Sort(out)
+	return out
+}
+
+func (bc botCounts) string() string {
+	var b strings.Builder
+	sortedKeys := sortMapKeys(bc)
+	for _, v := range sortedKeys {
+		b.WriteString(fmt.Sprintf("%d-%d", v, bc[v]))
+	}
+
+	return b.String()
+}
+
 // NB we start with 1 ore bot
 func newBotCounts() botCounts {
 	bc := make(botCounts)
@@ -82,6 +127,16 @@ func newBotCounts() botCounts {
 }
 
 type stockPiles map[resource]int
+
+func (sp stockPiles) string() string {
+	var b strings.Builder
+	sortedKeys := sortMapKeys(sp)
+	for _, v := range sortedKeys {
+		b.WriteString(fmt.Sprintf("%d-%d", v, sp[v]))
+	}
+
+	return b.String()
+}
 
 func newStockPiles() stockPiles {
 	sp := make(stockPiles)
@@ -97,8 +152,11 @@ type simulationState struct {
 	blueprint
 	elapsedTime int
 	stockPiles
-	enqueued resource
-	runTime  int
+	runTime int
+}
+
+func (state simulationState) string() string {
+	return fmt.Sprintf("%d-%s-%s", state.elapsedTime, state.botCounts.string(), state.stockPiles.string())
 }
 
 func newSimState(bp blueprint, runTime int) simulationState {
@@ -107,7 +165,6 @@ func newSimState(bp blueprint, runTime int) simulationState {
 		bp,
 		0,
 		newStockPiles(),
-		none,
 		runTime,
 	}
 }
@@ -126,7 +183,6 @@ func copyState(input simulationState) simulationState {
 		input.blueprint,
 		input.elapsedTime,
 		stockPilesCopy,
-		input.enqueued,
 		input.runTime,
 	}
 }
@@ -137,23 +193,11 @@ func (s *simulationState) harvest() {
 	}
 }
 
-func (s *simulationState) constructBot() {
-	if s.enqueued == none {
-		return
-	}
-	botType := s.enqueued
-	s.enqueued = none
-	s.botCounts[botType]++
-}
-
 func (s *simulationState) elapseTime(mins int) {
-	// we complete a harvest phase and a build phase
-	// every minute that elapses
 	for mins > 0 {
 		mins--
 		s.elapsedTime++
 		s.harvest()
-		s.constructBot()
 	}
 }
 
@@ -171,15 +215,16 @@ func (s simulationState) sated() bool {
 		s.botCounts[obsidian] == s.blueprint.geodeBot.obsidianCost
 }
 
-// TODO: bad modeling here
+// never build more of a particular kind of bot than can be
+// consumed by a single iteration
 func (s simulationState) canEnqueue(kind botKind) bool {
 	switch kind {
 	case ore:
-		return s.botCounts[ore] < 5
+		return s.botCounts[ore] < s.blueprint.botMax[ore]
 	case clay:
-		return s.botCounts[clay] < 5
+		return s.botCounts[clay] < s.blueprint.botMax[clay]
 	case obsidian:
-		return s.botCounts[clay] > 0 && s.botCounts[obsidian] < 5
+		return s.botCounts[clay] > 0 && s.botCounts[obsidian] < s.blueprint.botMax[obsidian]
 	case geode:
 		return s.botCounts[obsidian] > 0
 	default:
@@ -187,113 +232,114 @@ func (s simulationState) canEnqueue(kind botKind) bool {
 	}
 }
 
-func (s *simulationState) addBot(kind botKind) bool {
+func (s *simulationState) addBot(kind botKind) {
 	switch kind {
 	case ore:
 		missingOre := s.blueprint.oreBot.oreCost - s.stockPiles[ore]
 
-		for missingOre > 0 {
+		for missingOre > 0 && s.elapsedTime < s.runTime {
 			s.elapseTime(1)
 			missingOre = s.blueprint.oreBot.oreCost - s.stockPiles[ore]
 		}
 
-		if s.elapsedTime > s.runTime {
-			return false
+		// if we hit the end of the simulation time while we're waiting for enough
+		// ore to build the next bot, don't actually build the bot
+		if s.elapsedTime == s.runTime {
+			return
 		}
 
+		// otherwise, there's at least 1 iteration remaining before the simulation ends,
+		// so use that iteration to debit the cost of a bot, run the harvest for the iteration, and then add
+		// the newly-constructed bot to bot totals for this state frame.
 		s.stockPiles[ore] -= s.blueprint.oreBot.oreCost
-		s.enqueued = ore
-		return true
+		s.elapseTime(1)
+		s.botCounts[ore]++
 
 	case clay:
 		missingOre := s.blueprint.clayBot.oreCost - s.stockPiles[ore]
 
-		for missingOre > 0 {
+		for missingOre > 0 && s.elapsedTime < s.runTime {
 			s.elapseTime(1)
 			missingOre = s.blueprint.clayBot.oreCost - s.stockPiles[ore]
 		}
 
-		if s.elapsedTime > s.runTime {
-			return false
+		if s.elapsedTime == s.runTime {
+			return
 		}
 
 		s.stockPiles[ore] -= s.blueprint.clayBot.oreCost
-		s.enqueued = clay
-		return true
+		s.elapseTime(1)
+		s.botCounts[clay]++
 
 	case obsidian:
 		missingOre := s.blueprint.obsidianBot.oreCost - s.stockPiles[ore]
 		missingClay := s.blueprint.obsidianBot.clayCost - s.stockPiles[clay]
 		if missingOre > 0 || missingClay > 0 {
-			for missingClay > 0 || missingOre > 0 {
+			for (missingClay > 0 || missingOre > 0) && s.elapsedTime < s.runTime {
 				s.elapseTime(1)
 				missingOre = s.blueprint.obsidianBot.oreCost - s.stockPiles[ore]
 				missingClay = s.blueprint.obsidianBot.clayCost - s.stockPiles[clay]
 			}
 		}
-		if s.elapsedTime > s.runTime {
-			return false
+
+		if s.elapsedTime == s.runTime {
+			return
 		}
+
 		s.stockPiles[ore] -= s.blueprint.obsidianBot.oreCost
 		s.stockPiles[clay] -= s.blueprint.obsidianBot.clayCost
-		s.enqueued = obsidian
-		return true
+		s.elapseTime(1)
+		s.botCounts[obsidian]++
 
 	case geode:
 		missingOre := s.blueprint.geodeBot.oreCost - s.stockPiles[ore]
 		missingObsidian := s.blueprint.geodeBot.obsidianCost - s.stockPiles[obsidian]
 		if missingOre > 0 || missingObsidian > 0 {
-			for missingObsidian > 0 || missingOre > 0 {
+			for (missingObsidian > 0 || missingOre > 0) && s.elapsedTime < s.runTime {
 				s.elapseTime(1)
 				missingOre = s.blueprint.geodeBot.oreCost - s.stockPiles[ore]
 				missingObsidian = s.blueprint.geodeBot.obsidianCost - s.stockPiles[obsidian]
 			}
 		}
-		if s.elapsedTime > s.runTime {
-			return false
+
+		if s.elapsedTime == s.runTime {
+			return
 		}
+
 		s.stockPiles[ore] -= s.blueprint.geodeBot.oreCost
 		s.stockPiles[obsidian] -= s.blueprint.geodeBot.obsidianCost
-		s.enqueued = geode
-		return true
+		s.elapseTime(1)
+		s.botCounts[geode]++
+
 	default:
 		panic("bot enqueueing default case reached in error")
 	}
 }
 
-// 1. don't add a state frame for building any robots if current income-per-round is enough
-// to build 1 geode robot per turn
-// 2. track best theoretical max seen for given time elapsed. For each possible next frame, calculate
-// the theoretical max it could produce.  If it is less than the saved theoretical max, abandon the frame,
-// i.e. don't add it to the queue.
-
 // returns an array of all possible states for the beginning of the next minute
 func (s *simulationState) nextStates() []simulationState {
-	// this adds any bots enqueued by previous calls to this function
-	s.elapseTime(1)
-	next := []simulationState{}
+	nextStates := []simulationState{}
 
-	// once we're sated, we can calculate the max geodes programatically and skip a bunch of frames by
-	// pushing a frame with all time elapsed
 	if s.sated() {
 		newState := copyState(*s)
+		// fast-forward to end of simulation
 		newState.elapsedTime = s.runTime
+		// once we have enough bots online, the theoretical max is the
+		// same as the actual max for the given elapsed time.
 		newState.stockPiles[geode] = newState.theoreticalMax()
-		next = append(next, newState)
-		return next
+		nextStates = append(nextStates, newState)
+		return nextStates
 	}
-
-	// next = append(next, copyState(*s))
 
 	for botKind := range s.botCounts {
 		if s.canEnqueue(botKind) {
 			newState := copyState(*s)
 			newState.addBot(botKind)
-			next = append(next, newState)
+			nextStates = append(nextStates, newState)
 		}
 	}
 
-	return next
+	return nextStates
 }
 
 func intMax(a, b int) int {
@@ -308,38 +354,61 @@ func getMaxGeodes(totalRunTime int, bp blueprint) int {
 	baseState := newSimState(bp, totalRunTime)
 	q := []simulationState{baseState}
 	maxOpenedGeodes := 0
-	cache := map[int]int{}
-	loopcount := 0
+	cache := make(map[string]struct{})
 	for len(q) > 0 {
-		loopcount++
 		currState := q[0]
 		q = q[1:]
 
+		maxOpenedGeodes = intMax(maxOpenedGeodes, currState.stockPiles[geode])
+
+		// don't derive any future states from a current state with no
+		// remaining runtime
 		if currState.elapsedTime == totalRunTime {
-			maxOpenedGeodes = intMax(maxOpenedGeodes, currState.stockPiles[geode])
 			continue
 		}
 
-		// generating next states handles all updates due to elapsed time
+		// if a proposed state can't possibly outperform some state we've already
+		// seen, there's no need to check any of its possible future states
+		if maxOpenedGeodes > 0 && currState.theoreticalMax() <= maxOpenedGeodes {
+			continue
+		}
+
+		cacheKey := currState.string()
+
+		// if we've seen this exact state before, add no future states
+		if _, cacheHit := cache[cacheKey]; cacheHit {
+			continue
+		}
+
+		cache[cacheKey] = struct{}{}
+
 		nextStates := currState.nextStates()
 
 		lo.ForEach(nextStates, func(sim simulationState, idx int) {
-			cachedMax, exists := cache[sim.elapsedTime]
-			thMax := sim.theoreticalMax()
-			if exists && thMax < cachedMax {
-				return
-			}
-			cache[sim.elapsedTime] = thMax
 			q = append(q, sim)
 		})
 	}
 	return maxOpenedGeodes
 }
 
-func main() {
-	fmt.Println("hello")
+func solvePartOne(r io.Reader) int {
+	scanner := bufio.NewScanner(r)
+	qualitySum := 0
+	for scanner.Scan() {
+		bp := parseBlueprint(scanner.Text())
+		qualitySum += getMaxGeodes(24, bp) * bp.id
+	}
+	return qualitySum
 }
 
-func solve() int {
-	return 42
+func solvePartTwo(r io.Reader) int {
+	scanner := bufio.NewScanner(r)
+	product := 1
+	linesRead := 0
+	for scanner.Scan() && linesRead < 3 {
+		bp := parseBlueprint(scanner.Text())
+		product *= getMaxGeodes(32, bp)
+		linesRead++
+	}
+	return product
 }
--- 19_not_enough_minerals/main_test.go
+++ 19_not_enough_minerals/main_test.go
@@ -1,24 +1,12 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
-// var testInput = `Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
-// Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.`
-
-func TestParseBlueprint(t *testing.T) {
-	given := "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian."
-	want := blueprint{
-		1,
-		oreCost{4},
-		clayCost{2},
-		obsidianCost{3, 14},
-		geodeCost{2, 7},
-	}
-	got := parseBlueprint(given)
-	if got != want {
-		t.Fatalf("expected %d, received %d", want, got)
-	}
-}
+var testInput = `Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.`
 
 func TestGetMaxGeodes(t *testing.T) {
 	given := "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian."
@@ -28,9 +16,18 @@ func TestGetMaxGeodes(t *testing.T) {
 		t.Fatalf("expected %d, received %d", want, got)
 	}
 }
+
+func TestGetMaxGeodesTwo(t *testing.T) {
+	given := "Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian."
+	want := 62
+	got := getMaxGeodes(32, parseBlueprint(given))
+	if got != want {
+		t.Fatalf("expected %d, received %d", want, got)
+	}
+}
 func TestSolve(t *testing.T) {
 	want := 33
-	got := solve()
+	got := solvePartOne(strings.NewReader(testInput))
 	if got != want {
 		t.Fatalf("expected %d, received %d", want, got)
 	}
